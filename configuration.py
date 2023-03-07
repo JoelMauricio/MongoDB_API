059113fd3a5603c36832224a8eac056fdb2cb0da
Rohtgib <vicgmezp@gmail.com>
2023-03-07 19:33:20 -0400

    Cambios en .gitignore y configuration.py, dar formato a restaurant-reviews.json

--- configuration.py
+++ configuration.py
@@ -1,3 +1,4 @@
+#Conexiones de datos
 LOCAL_CONNECTION_STRING='mongodb://localhost:27017/NOSQL'
 CLOUD_CONNECTION_STRING='mongodb+srv://vicgmezp:<password>@cluster0.hgpn76k.mongodb.net/?retryWrites=true&w=majority'
 #Puerto para FastAPI
