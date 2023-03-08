53c8366bebc2f7c6de9dc516fe749cf6a2b71a36
Joel <1105039@est.intec.edu.do>
2023-03-07 21:18:37 -0400

    se agregó configuración de puerto de la api

--- mainAPI.py
+++ mainAPI.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 import json
+import configuration as C
+from uvicorn import run 
 
 app = FastAPI()
 
-with open("./data/restaurants-reviews.json", "r") as f:
+with open("./data/restaurants.json", "r") as f:
     data = json.load(f)
 
 @app.get("/")
 def index():
     return data
+
+if __name__ == "__main__":
+    run(app, port=C.PORT)
