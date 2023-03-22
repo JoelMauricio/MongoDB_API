977093f104cf0fb73e92884546b3a3087170acc1
Joel <1105039@est.intec.edu.do>
2023-03-22 01:38:02 -0400

    Added pytest test

---
+++ test_queries.py
@@ -0,0 +1,22 @@
+import queries as q
+import pytest
+
+
+def test_consulta1():
+    assert q.consulta1() != None
+
+
+def test_consulta2():
+    assert q.consulta2() != None
+
+
+def test_consulta3():
+    assert q.consulta3() != None
+
+
+def test_consulta4():
+    assert q.consulta4() != None
+
+
+def test_consulta5():
+    assert q.consulta5() != None
